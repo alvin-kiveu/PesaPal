e614a78851f7498c7ae994dbfc8d6eda8df5cef6
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-09-01 02:03:12 +0300

    adding new data

--- acesstoken.php
+++ acesstoken.php
@@ -2,12 +2,12 @@
 define('APP_ENVIROMENT', 'live'); // sandbox or live
 if(APP_ENVIROMENT == 'sandbox'){
     $apiUrl = "https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken"; // Sandbox URL
-    $consumerKey = "qkio1BGGYAXTu2JOfm7XSXNruoZsrqEW";
-    $consumerSecret = "osGQ364R49cXKeOYSpaOnT++rHs==";
+    $consumerKey = "";
+    $consumerSecret = "";
 }elseif(APP_ENVIROMENT == 'live'){
     $apiUrl = "https://pay.pesapal.com/v3/api/Auth/RequestToken"; // Live URL
-    $consumerKey = "YZ+GAXagwzaFhpnGLStAfUyzt7aKQle2";
-    $consumerSecret = "g8HaRhWxuYrTcYOWNm0CunTvFPs=";
+    $consumerKey = "";
+    $consumerSecret = "";
 }else{
     echo "Invalid APP_ENVIROMENT";
     exit;
